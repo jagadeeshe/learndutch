3af251d546e1ed4472e18aea711fdbbfc800f203
Jagadeesh Babu <jagadeeshbabue@gmail.com>
2011-05-25 08:48:13 +0200

    refactored to use standard template naming convention for forms

--- src/learndutch/mainapp/views.py
+++ src/learndutch/mainapp/views.py
@@ -8,14 +8,14 @@ from models import Word, Noun, Verb, WORD_TYPE_NOUN, WORD_TYPE_VERB
 
 class CreateNounView(CreateView):
     form_class = NounForm
-    template_name = "add_noun.html"
+    template_name = "noun_form.html"
     success_url = "/add-noun/"
     initial = {'word_type': WORD_TYPE_NOUN}
 
 
 class CreateVerbView(CreateView):
     form_class = VerbForm
-    template_name = "add_verb.html"
+    template_name = "verb_form.html"
     success_url = "/add-verb/"
     initial = {'word_type': WORD_TYPE_VERB}
 
