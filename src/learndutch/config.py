b8257b04cc805b1101ee4471812133f0e07e5729
Jagadeesh Babu <jagadeeshbabue@gmail.com>
2011-06-19 19:23:13 +0200

    discover deploy_root at startup

--- src/learndutch/config.py
+++ src/learndutch/config.py
@@ -3,10 +3,11 @@ Created on May 13, 2011
 
 @author: jagadeesh
 '''
-
-
-# Specify abosolute path where the project is deployed
-DEPLOY_ROOT = ''
+import os
 
 def get_full_path(name):
-    return "%s%s" % (DEPLOY_ROOT, name)
+    path = os.path.abspath(__file__)
+    dir_path = os.path.dirname(path)
+    parts = dir_path.split(os.path.sep)
+    deploy_root = os.path.sep.join(parts[:-2])
+    return os.path.join(deploy_root, name)
